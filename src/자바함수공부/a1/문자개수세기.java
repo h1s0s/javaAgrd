e1d04da70d07e17adeaa4df4d8092a955ddb6f95
한상선 <hs94122@naver.com>
2023-09-21 20:57:39 +0900

    자바 함수 공부

---
+++ src/자바함수공부/a1/문자개수세기.java
@@ -0,0 +1,34 @@
+package 자바함수공부.a1;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class 문자개수세기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(countDuplicateCaracters1(str));
+        System.out.println(countDuplicateCaracters2(str));
+    }
+
+    public static Map<Character, Integer> countDuplicateCaracters1(String str){
+        Map<Character, Integer> result = new HashMap<>();
+         for (int i=0; i<str.length(); i++){
+             char ch = str.charAt(i);
+             result.compute(ch, (k,v) -> (v==null) ? 1 : ++v);//ch 키에 값 증가, 키에 값이 null이면 1, 아니면 ++
+         }
+        return result;
+    }
+
+    public static Map<Character, Long> countDuplicateCaracters2(String str){
+        Map<Character, Long> result = str.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+
+        return result;
+    }
+
+
+}
