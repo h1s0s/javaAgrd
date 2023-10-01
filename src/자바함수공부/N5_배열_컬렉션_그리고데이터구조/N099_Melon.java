e516867b98f13e613fddea7173b931188da95357
한상선 <hs94122@naver.com>
2023-10-01 11:01:34 +0900

    자바 함수 공부

--- src/자바함수공부/N5_배열_컬렉션_그리고데이터구조/N099_Melon.java
+++ src/자바함수공부/N5_배열_컬렉션_그리고데이터구조/N099_Melon.java
@@ -1,9 +1,15 @@
 package 자바함수공부.N5_배열_컬렉션_그리고데이터구조;
 
 public class N099_Melon {
+    String type;
     int weight;
 
     public N099_Melon() {
+    }
+
+    public N099_Melon(String type, int weight) {
+        this.type = type;
+        this.weight = weight;
     }
 
     public N099_Melon(int weight) {
@@ -14,6 +20,14 @@ public class N099_Melon {
         return weight;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public void setWeight(int weight) {
         this.weight = weight;
     }
@@ -21,7 +35,8 @@ public class N099_Melon {
     @Override
     public String toString() {
         return "N099_Melon{" +
-                "weight=" + weight +
+                "type='" + type + '\'' +
+                ", weight=" + weight +
                 '}';
     }
 }
